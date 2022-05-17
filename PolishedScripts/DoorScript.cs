9f863f889acd047f0e7cf52584c384078e5387a1
ReversedCombos <49067277+ReversedCombos@users.noreply.github.com>
2022-05-17 00:06:28 -0500

    Update DoorScript.cs

--- PolishedScripts/DoorScript.cs
+++ PolishedScripts/DoorScript.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 [RequireComponent(typeof(RayCastHandeler))]
 
+//Refer to "RayCastHandeler" for how the script is called
+//Plays an animation for doors based on the open/close state
+
 public class DoorScript : MonoBehaviour
 {
     [SerializeField] private bool DoorOpen;
