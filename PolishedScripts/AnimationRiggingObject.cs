f71be5d3eac03b605813f4eb159e48c922fe7bba
ReversedCombos <49067277+ReversedCombos@users.noreply.github.com>
2022-05-16 23:02:01 -0500

    Update AnimationRiggingObject.cs

--- PolishedScripts/AnimationRiggingObject.cs
+++ PolishedScripts/AnimationRiggingObject.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
 
+//This is for Animation Rigging based off of the active state of an object. I use this for weapon animation rigging
+//when the weapon is turned on, lerp the animation rig to 1 - When it is turned off, lerp it to 0
+
 public class AnimationRiggingObject : MonoBehaviour
 {
     [SerializeField] private GameObject Weapon;
